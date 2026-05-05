4eca04ec1a7f61d7158c18c0ba21a2ac8820142f
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-05 14:54:18 +0700

    feat: add event for login google

---
+++ auth-service/src/main/java/iuh/fit/authservice/event/AccountRegisteredEvent.java
@@ -0,0 +1,9 @@
+package iuh.fit.authservice.event;
+
+public record AccountRegisteredEvent(
+        String accountId,
+        String email,
+        String fullName,
+        String phoneNumber
+) {
+}
