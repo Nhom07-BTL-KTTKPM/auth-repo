141cf001adf41d1a1d513765b4a93663a615a5b2
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-05 15:12:01 +0700

    fix: fix error test for ci

--- auth-service/src/test/java/iuh/fit/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/iuh/fit/authservice/AuthServiceApplicationTests.java
@@ -2,9 +2,30 @@ package iuh.fit.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
+
+    @MockitoBean
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockitoBean
+    private ReactiveRedisConnectionFactory reactiveRedisConnectionFactory;
+
+    @MockitoBean
+    private ProxyManager<String> proxyManager;
+
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
